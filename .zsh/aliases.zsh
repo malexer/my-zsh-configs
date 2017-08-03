52cffe3d4ccececaef309af9d739da139ed60f1c
Oleksii Markov <alex@markovs.me>
2017-08-03 12:12:28 +0300

    Added gsn alias (git status -uno)

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,3 +2,4 @@ alias ccat=colorize
 
 # git
 alias glg="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)<%an>%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias gsn="git status -uno"
