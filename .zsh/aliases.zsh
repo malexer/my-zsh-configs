bd75be190f1fd654abf0de698defc1db26fafe62
Oleksii Markov <omarkov@cloudmade.com>
2020-03-18 18:46:36 +0200

    Added alias "l" for exa

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -3,3 +3,6 @@ alias ccat=colorize
 # git
 alias glg="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)<%an>%C(reset)%C(bold yellow)%d%C(reset)' --all"
 alias gsn="git status -uno"
+
+# exa (ls replacement)
+alias l="exa --long --git --group-directories-first -F"
