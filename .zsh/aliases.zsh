94b9c081c5829b5fbec1e6935a3ba407f20a4a9e
Oleksii Markov <omarkov@cloudmade.com>
2021-02-11 13:13:25 +0200

    Added alias for wttr

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,8 +1,10 @@
-alias ccat=colorize
-
 # git
 alias glg="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)<%an>%C(reset)%C(bold yellow)%d%C(reset)' --all"
 alias gsn="git status -uno"
 
 # exa (ls replacement)
 alias l="exa --long --git --group-directories-first -F"
+
+# info
+alias wttr="curl wttr.in/Kyiv"
+alias wttr2="curl v2.wttr.in/Kyiv"
